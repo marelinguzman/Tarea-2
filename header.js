e8dea9ad4d02d105dd60d1c2b91f9da4ba14a685
marelinguzman <126489728+marelinguzman@users.noreply.github.com>
2023-08-09 20:24:46 -0600

    Update header.js

--- header.js
+++ header.js
@@ -2,8 +2,8 @@ let footer_DOM = document.querySelector(".footer");
 
 let footer = `
     <div class="nodo-footer">
-        <span>@jefuentes</span>
-        <a href="https://github.com/jefuentes80" target="_blank">
+        <span>@mcguzman</span>
+        <a href="https://github.com/marelinguzman" target="_blank">
             <img src="https://cdn-icons-png.flaticon.com/512/25/25231.png">
             </a>
         </div>
